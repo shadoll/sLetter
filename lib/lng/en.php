3ab239f6b58143ff6764e3345abca4e7bf467084
sHa <sha@shadoll.com>
2018-02-12 02:56:07 +0200

    vomposer rebase

---
+++ lib/lng/en.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * @author sHa <sha@shadoll.com>
+ * @package sLetter
+ * @version 18.2.12
+ *
+ */
+
+return [
+	'subject' => '📨 Letter from site',
+	'name' => 'From',
+	'phone' => 'Phone',
+	'email' => 'Email',
+	'referer' => 'Referer',
+	'senderIP' => 'Sender IP',
+	'flag' => 'Flag',
+	'country' => 'Country',
+	'region' => 'Region',
+	'city' => 'City',
+	'provider' => 'Internet Provider',
+];
